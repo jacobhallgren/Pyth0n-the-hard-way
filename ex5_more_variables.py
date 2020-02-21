1ec326b6060708b6b0f7b41718571ca8588e6def
Jacob Hällgren <jacob.hallgren@telecomputing.se>
2020-02-21 10:32:12 +0100

    add edit math in ex5

--- ex5_more_variables.py
+++ ex5_more_variables.py
@@ -5,9 +5,10 @@ my_weight = 81
 my_eyes = 'green'
 my_teeth = 'White'
 my_hair = 'brown'
+my_height_in_inch = my_height / 2.54  
 
 print(f"Let's talk about {my_name}")
-print(f"He's {my_height} cm tall.")
+print(f"He's {my_height} cm tall. And thats is {my_height_in_inch}, inches ")
 print(f"He's {my_weight} kg heavy")
 print(f"He's got {my_eyes} eyes and {my_hair} hair.")
 print(f"His teeth are usually {my_teeth} depending ont he coffee")
