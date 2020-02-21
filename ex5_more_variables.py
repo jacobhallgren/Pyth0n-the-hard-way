e101ca04e750b4138d2160ed3d1038438c20b672
Jacob Hällgren <jacob.hallgren@telecomputing.se>
2020-02-21 09:47:55 +0100

    add files.py

---
+++ ex5_more_variables.py
@@ -0,0 +1,16 @@
+my_name = 'Jacob Hallgren'
+my_age = 28
+my_height = 174
+my_weight = 81
+my_eyes = 'green'
+my_teeth = 'White'
+my_hair = 'brown'
+
+print(f"Let's talk about {my_name}")
+print(f"He's {my_height} cm tall.")
+print(f"He's {my_weight} kg heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending ont he coffee")
+
+total = my_age + my_height + my_weight
+print(f"if i add {my_age}, {my_height}, and I get {total}.")
