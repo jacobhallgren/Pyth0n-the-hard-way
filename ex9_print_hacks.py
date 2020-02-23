7e71a82f1677a7e16904352ecb4e0e4f33a46673
Jacob Hällgren <jacobhallgren@Jacobs-MacBook-Pro.local>
2020-02-23 18:59:31 +0100

    added file ex9

---
+++ ex9_print_hacks.py
@@ -0,0 +1,15 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days", days)
+print("Here are the months", months)
+
+print("""
+Here I put many text, very nice.com
+And here
+Also here
+Will Kenny see this.
+One more line then.
+And the last one
+
+""")
