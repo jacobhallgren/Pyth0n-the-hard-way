0815849b050450309cedb3e365968c3bd68cde6b
Jacob Hällgren <jacob.hallgren@telecomputing.se>
2020-02-25 11:56:37 +0100

    added file ex13

---
+++ ex13_passing_parameters.py
@@ -0,0 +1,12 @@
+from sys import argv
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second varable is:", second)
+print("Your third variable is:", third)
+
+
+textin = input("type some text")
+print(textin)
+
