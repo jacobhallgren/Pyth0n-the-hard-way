dd7b7ba03bd852e41984b3ae637a0a46799737e6
Jacob Hällgren <jacobhallgren@Jacobs-MacBook-Pro.local>
2020-03-06 22:36:15 +0100

    added 22

---
+++ ex22.py
@@ -0,0 +1,32 @@
+##Notes
+
+#create a function
+
+def NAME(value_A, value_b):
+    print("hello")
+
+#create variabel
+varibelname = 20
+
+#print something
+print("This is nice", varibelname)
+
+#Passing varibles
+from sys import argv
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second varable is:", second)
+print("Your third variable is:", third)
+
+
+#print int and string
+print(f"So, you're {age} old, {height} tall and {weight} heavy")
+
+
+#get input
+age = input("put in age")
+print(age)
+
+
