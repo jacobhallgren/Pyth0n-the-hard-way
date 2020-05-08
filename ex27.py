93baee76e847ec83cd21355cf07044cb6c22d203
Jacob Hällgren <jacob.hallgren@telecomputing.se>
2020-05-08 13:22:04 +0200

    added two workflows

---
+++ ex27.py
@@ -0,0 +1,8 @@
+# true and true = true
+# true and false = false
+# false and false = false 
+# != not equal 
+# == equal
+#>=greater-than equal
+# 
+# 
---
+++ ex28.py
@@ -0,0 +1,6 @@
+print(True and True)
+print (True and False)
+print(4 > 3)
+print(3 >= 3) #3 bigger than equal 
+print(3 == 3) #equal
+print(3 != 2) #not equal
