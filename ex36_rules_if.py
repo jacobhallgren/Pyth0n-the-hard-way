4a7f617ca479f459a23566fa5d02948b990b2f6f
Jacob Hällgren <jacob.hallgren@telecomputing.se>
2020-06-01 22:40:10 +0200

    ex36

---
+++ ex36_rules_if.py
@@ -0,0 +1,6 @@
+#Rules of if-statements
+#All if should end with and else:
+#Use while loop to loop forever
+#Use for loop for all other kind of looping.
+
+#Try not to debug the whole program, use print to print varibables and see where they go wrong.
