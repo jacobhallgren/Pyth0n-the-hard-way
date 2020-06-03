5c45375916f48c1fcc041a4c1675328ece84c29b
Jacob Hällgren <jacobhallgren@MacBook-Pro.local>
2020-06-03 22:48:55 +0200

    added ex37

---
+++ ex37_review.py
@@ -0,0 +1,9 @@
+x = hello # string
+i = 100 #store number, integers
+a = None # no value 
+b = 10.230 # float
+
+j = [1, 2, 3, 4] #list
+k = {'x' : 1, 'y' :2} # Dictionary, dicts
+
+3 < 4 # 3 less than 4
