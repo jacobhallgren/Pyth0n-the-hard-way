cd2040205922dc1d4feb3f2fef3ec92d0ee52c2d
Jacob Hällgren <jacobhallgren@MacBook-Pro.local>
2020-06-15 14:20:53 +0200

    added 41

---
+++ ex41_objects.py
@@ -0,0 +1,4 @@
+#class = Tell python type of thing 
+#object = Basic kind of thing and any instance of something
+#instance = what you get when you tell python to create a class
+#self = Inside a function in a class, self is a variable for the class/instance
